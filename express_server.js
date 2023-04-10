e96693896ac0753bdd5fd721195852f2e4ec4f46
Shawn He <shawnhe1991@gmail.com>
2023-04-10 14:37:26 -0400

    complete adding long url to dictionary

--- express_server.js
+++ express_server.js
@@ -25,8 +25,16 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  const foundKey = Object.keys(urlDatabase).find((key) => urlDatabase[key] === req.body.longURL);// find if long url already exists in dictionary
+  console.log(foundKey);
+  if (foundKey === undefined) {
+    urlDatabase[generateRandomString()] = req.body.longURL;
+    return res.send("ok");// Respond with 'Ok' (we will replace this)
+  } else {
+    res.send("its already there");
+  }
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+
 });
 
 app.get("/urls/new", (req, res) => {
