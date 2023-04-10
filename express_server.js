c97c5b8ca39fc122c0a94bfe9cc8817223da5d5a
Shawn He <shawnhe1991@gmail.com>
2023-04-10 13:38:42 -0400

    complete add new portion

--- express_server.js
+++ express_server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6)
+}
+
 app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,6 +24,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   // alt way of doing things
@@ -26,6 +42,7 @@ app.get("/urls/:id", (req, res) => {
   // const templateVars = { id: shortURL, longURL};
   res.render("urls_show", templateVars);
 });
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
