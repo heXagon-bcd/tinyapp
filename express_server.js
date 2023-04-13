d69b9c58af4a97947a2e54580479c3272232a2a8
Shawn He <shawnhe1991@gmail.com>
2023-04-13 11:26:32 -0400

    setup merge

--- express_server.js
+++ express_server.js
@@ -33,7 +33,7 @@ app.post("/urls", (req, res) => {
   } else {
     res.send("its already there");
   }
-  console.log(req.body); // Log the POST request body to the console
+  console.log(req.body); // Log the POST request body to the console//
 
 });
 
